128fd6bbbba8bb67c15a3befc61c1563cc09fe98
Kevin Deng <sxzz@sxzz.moe>
2026-08-22 15:43:16 +0900

    feat: update dependencies and add analytics injection

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
+import { inject } from '@vercel/analytics'
 import { createVaporApp } from 'vue'
 import App from './App.vue'
 import '@unocss/reset/tailwind.css'
 import 'uno.css'
 import './styles.css'
 
+inject()
 createVaporApp(App).mount('#root')
