edfce255cc3754c995b02b69e77b6a3971802162
Kevin Deng <sxzz@sxzz.moe>
2026-09-05 15:50:05 +0900

    chore: upgrade deps

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,9 +2,3 @@ declare module '*.yaml' {
   const value: import('./types').MapData
   export default value
 }
-
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent
-  export default component
-}
